f6492d40646f14f7e7738f9517419dd3e3130b45
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-10 10:17:38 +0530

    rendered slots flatlist

--- src/screens/SlotsScreen.js
+++ src/screens/SlotsScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { connect } from 'react-redux';
 import { fetchSlots } from '../store/actions/slotActions';
+import { FlatList } from 'react-native-gesture-handler';
 
 class SlotsScreen extends Component {
     componentDidMount() {
@@ -16,9 +17,14 @@ class SlotsScreen extends Component {
             (
                 slotsData.slots && slotsData.slots.length ?
                 (
-                    <View>
-                        <Text>Slots Screen</Text>
-                    </View>
+                    <FlatList
+                        data={slotsData.slots}
+                        keyExtractor={(slot, i) => i}
+                        renderItem={(item, index) => {
+                            console.log('item', item.item.slotTimeString);
+                            return (<View><Text>{item.item.slotTimeString}</Text></View>)
+                        }}
+                    />
                 ): 
                 (
                     <View>
